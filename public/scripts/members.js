079a04bf41efb8ab3af1ce1df34fb5d94d230b4c
Kyle <kyleadriansantos1006@gmail.com>
2024-07-19 00:16:43 +0800

    members edit button done

---
+++ public/scripts/members.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    $('.edit-btn').on('click', function() {
+        // Hide the edit button
+        $(this).hide();
+
+        // Replace spans with input fields
+        $('.profile-details .info-item .value').each(function() {
+            var $this = $(this);
+            var input = $('<input>', {
+                type: 'text',
+                placeholder: $this.text(),
+                id: $this.attr('id')
+            });
+            $this.replaceWith(input);
+        });
+
+        // Replace dispute link with Save Changes and Discard Changes buttons
+        var $disputeLink = $('#blacklist');
+        $disputeLink.hide();
+
+        var saveBtn = $('<a>', {
+            text: 'Save Changes',
+            class: 'save-changes',
+            href: '#'
+        });
+
+        var discardBtn = $('<a>', {
+            text: 'Discard Changes',
+            class: 'discard-changes',
+            href: '#'
+        });
+
+        $('.footer').append(saveBtn).append(discardBtn);
+
+        // Add event listeners for Save Changes and Discard Changes buttons
+        $('.save-changes').on('click', function(event) {
+            event.preventDefault();
+            $('.profile-details .info-item input').each(function() {
+                var $this = $(this);
+                var span = $('<span>', {
+                    class: 'value',
+                    text: $this.val()
+                });
+                $this.replaceWith(span);
+            });
+            resetEdit();
+        });
+
+        $('.discard-changes').on('click', function(event) {
+            event.preventDefault();
+            resetEdit();
+        });
+    });
+
+    function resetEdit() {
+        // Replace spans with input fields
+        $('.profile-details .info-item input').each(function() {
+            var $this = $(this);
+            var input = $('<span>', {
+                class: 'value',
+                id: $this.attr('id'),
+                text: $this.attr('placeholder'),
+            });
+            $this.replaceWith(input);
+        });
+
+        $('.save-changes').remove();
+        $('.discard-changes').remove();
+        $('#blacklist').show();
+        $('.edit-btn').show();
+    }
+});
